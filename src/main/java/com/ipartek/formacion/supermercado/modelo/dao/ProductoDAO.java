6bac5418e9b7b15781bd741a02f497dbc6acc2ce
Curso <Curso@Ipartek-TOSH03.ipartekaula.com>
2019-12-20 13:58:55 +0100

    CRUD

--- src/main/java/com/ipartek/formacion/supermercado/modelo/dao/ProductoDAO.java
+++ src/main/java/com/ipartek/formacion/supermercado/modelo/dao/ProductoDAO.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,11 +15,14 @@ public class ProductoDAO implements IDAO<Producto>{
 
 	private static ProductoDAO INSTANCE;
 
-	//para la consulta a la base de datos:
+	//ctes para la consulta a la base de datos:
 	private static final String SQL_GET_ALL = "SELECT id, nombre FROM producto ORDER BY id DESC LIMIT 500;";
+	private static final String SQL_INSERT = "INSERT INTO producto (nombre) VALUES (?);";
+	private static final String SQL_GET_BY_ID = "SELECT p.id, p.nombre FROM producto as p WHERE p.id = ?;";
+	private static final String SQL_DELETE = "DELETE FROM producto WHERE id = ?;";
+	private static final String SQL_UPDATE = "UPDATE producto SET nombre= ? WHERE id = ?;";
+
 	
-		
-	//capar para que no se pueda hacer new de esta clase
 	private ProductoDAO() {
 		super();
 	}
@@ -58,27 +62,110 @@ public class ProductoDAO implements IDAO<Producto>{
 
 		return lista;
 	}
-
-	@Override
-	public Producto getById(int id) { 
-		return null;
-	}
-		
-
-	@Override
-	public Producto delete(int id) throws Exception {
-		return null;
-	}
-
-	@Override
-	public Producto update(Producto pojo, int id) throws Exception {
-		return null;
-	}
-
-
+	
+	
 	@Override
 	public Producto create(Producto pojo) throws Exception {
-		return null;
+		
+		try (Connection con = ConnectionManager.getConnection(); PreparedStatement pst = con.prepareStatement(SQL_INSERT, Statement.RETURN_GENERATED_KEYS)) {
+			pst.setString(1, pojo.getNombre());
+			
+			int affectedRows = pst.executeUpdate();
+			if (affectedRows == 1) {
+				// conseguimos el ID que acabamos de crear
+				ResultSet rs = pst.getGeneratedKeys();
+				if (rs.next()) {
+					pojo.setId(rs.getInt(1));
+				}
+			}
+				
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return pojo;		
+	}
+
+	
+	@Override
+	public Producto getById(int id) { 
+		Producto resul = new Producto();
+		
+		try (Connection con = ConnectionManager.getConnection();
+				PreparedStatement pst = con.prepareStatement(SQL_GET_BY_ID)) {
+
+			pst.setInt(1, id);
+
+			try (ResultSet rs = pst.executeQuery()) {
+				if (rs.next()) {
+					resul = mapper(rs);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return resul;
 	}
 	
+	
+	@Override
+	public Producto delete(int id) throws Exception {
+		Producto resul = new Producto();
+		
+		try (Connection con = ConnectionManager.getConnection(); PreparedStatement pst = con.prepareStatement(SQL_DELETE);) {
+
+			pst.setInt(1, id);
+
+			int affetedRows = pst.executeUpdate();
+			if (affetedRows == 1) {
+				resul = null; //¿?  
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return resul;
+	}
+	
+	
+	@Override
+	public Producto update(Producto pojo, int id) throws Exception {
+		Producto resul = new Producto();
+		
+		try (Connection con = ConnectionManager.getConnection(); PreparedStatement pst = con.prepareStatement(SQL_UPDATE);) {
+
+			pst.setString(1, pojo.getNombre());
+			pst.setInt(2, pojo.getId());
+
+			int affetedRows = pst.executeUpdate();
+			if (affetedRows == 1) {
+				resul = null; //¿?  
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return resul;
+	}
+
+		
+	private Producto mapper(ResultSet rs) throws SQLException {
+		
+		Producto p = new Producto();
+		p.setId(rs.getInt("id"));
+		p.setNombre(rs.getString("nombre"));
+			
+		/*
+		Rol rol = new Rol();
+		rol.setId( rs.getInt("id_rol"));
+		rol.setNombre( rs.getString("nombre_rol"));
+		p.setRol(rol);
+		*/
+		
+		return p;
+	}
+
 }
