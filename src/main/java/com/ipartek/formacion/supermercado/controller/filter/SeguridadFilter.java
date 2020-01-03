7b925e3ca5db88e9b437ced1acc7600fde4f0ae0
Curso <Curso@Ipartek-TOSH03.ipartekaula.com>
2020-01-03 09:18:52 +0100

    filtro seguridad backoffice-admin

--- src/main/java/com/ipartek/formacion/supermercado/controller/filter/SeguridadFilter.java
+++ src/main/java/com/ipartek/formacion/supermercado/controller/filter/SeguridadFilter.java
@@ -1,14 +1,11 @@
 package com.ipartek.formacion.supermercado.controller.filter;
 
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Map;
 
 import javax.servlet.DispatcherType;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
