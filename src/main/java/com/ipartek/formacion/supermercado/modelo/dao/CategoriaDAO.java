f7aec22894a2fd06bd2ab08e4ca43477fd5ec0ab
Curso <Curso@Ipartek-TOSH03.ipartekaula.com>
2020-01-09 10:15:17 +0100

    getall, insert, delete, getbyid y update con PA en CategoriaDAO

--- src/main/java/com/ipartek/formacion/supermercado/modelo/dao/CategoriaDAO.java
+++ src/main/java/com/ipartek/formacion/supermercado/modelo/dao/CategoriaDAO.java
@@ -2,7 +2,6 @@ package com.ipartek.formacion.supermercado.modelo.dao;
 
 import java.sql.CallableStatement;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -41,13 +40,15 @@ public class CategoriaDAO implements ICategoriaDAO {
 
 			// ejecutamos dentro de un try para que se cierre después de ejecutarse:
 			try (ResultSet rs = cs.executeQuery()) {
-				// TODO mapper
 
 				while (rs.next()) {
+					/*
 					Categoria c = new Categoria();
 					c.setId(rs.getInt("id"));
 					c.setNombre(rs.getString("nombre"));
 					registros.add(c);
+					*/
+					registros.add(mapper(rs));
 				}
 
 			}
@@ -62,12 +63,10 @@ public class CategoriaDAO implements ICategoriaDAO {
 
 	@Override
 	public Categoria getById(int id) {
-		// TODO tipo getAll pero con 1 parámetro
 		
-		//Categoria registro = null;
+		LOG.trace("Recuperar la categoría según su id" + id);
+		
 		Categoria registro = new Categoria();
-
-		LOG.trace("Recuperar la categoría según su id");
 
 		// conseguimos la conexión (getConnection) y hacemos la llamada al procedimiento almacenado (prepareCall):
 		try (Connection con = ConnectionManager.getConnection();
@@ -79,18 +78,18 @@ public class CategoriaDAO implements ICategoriaDAO {
 
 			// ejecutamos dentro de un try para que se cierre después de ejecutarse:
 			try (ResultSet rs = cs.executeQuery()) {
-				// TODO mapper
 
-				while (rs.next()) {
+				if (rs.next()) { //como sólo va a haber un resultado podríamos poner un while o un if
 					
-					registro.setId(rs.getInt("id"));
-					registro.setNombre(rs.getString("nombre"));
+					registro = mapper(rs);
 					
+				}else {
+					registro = null; //no podemos lanzar excepción porque no lo definimos así en IDAO
 				}
-
+					
 			}
 
-		} catch (SQLException e) {
+		} catch (Exception e) {
 			LOG.error(e);
 		}
 
@@ -101,9 +100,13 @@ public class CategoriaDAO implements ICategoriaDAO {
 	@Override
 	public Categoria delete(int id) throws Exception {    //eliminando categoría
 
-		Categoria registro = null;
+		LOG.trace("Eliminar categoría por id" + id);
 		
-		LOG.trace("Eliminar categoría");
+		//recuperamos la categoría antes de eliminarla:
+		Categoria registro = getById(id);
+		if(registro == null) {
+			throw new Exception("Registro no encontrado " + id);
+		}
 
 		// conseguimos la conexión (getConnection) y hacemos la llamada al procedimiento almacenado (prepareCall):	
 		try (Connection con = ConnectionManager.getConnection();
@@ -112,20 +115,13 @@ public class CategoriaDAO implements ICategoriaDAO {
 			//parámetro de entrada (1era y única ?):
 			cs.setInt(1, id);
 			LOG.debug(cs);
-			
-			//obtenemos el id antes de eliminarlo:
-			registro = this.getById(id);
 
 			//eliminamos el producto:
-			int affetedRows = cs.executeUpdate();
-			if (affetedRows != 1) {
-				registro = null; //eliminamos
-				throw new Exception("No se puede eliminar " + registro);
-			}
+			cs.executeUpdate();
 
-		} catch (Exception e) {
+		} /* catch (Exception e) {
 			LOG.error(e); 
-		}
+		} //quitamos este catch para que no se capture la excepción si se quiere eliminar una categoría con porductos asociados */ 
 		
 		return registro;
 	
@@ -133,31 +129,36 @@ public class CategoriaDAO implements ICategoriaDAO {
 
 	@Override
 	public Categoria update(Categoria pojo, int id) throws Exception {
-		LOG.trace("Modificar categoría");
+		
+		LOG.trace("Modificar categoría por id " + id + " " + pojo);
+		
+		Categoria registro = pojo; //IMPORTANTE: 
+								   //toda variable primitiva si se hace una copia se copia el valor. 
+								   //Toda variable que no sea primitiva, se pasa una copia de una dirección de memoria cuando se iguala, por eso, resgistro y pojo tienen el mismo valor
+								   //Usaremos el método clone si queremos copiar el contenido de 2 variables no primitivas sin que apunten a la misma dir de memoria
+								   //cualquier variable primitiva se pasa una copia por valor, por lo que fuera de esa función no ve alterado su valor
 
 		// conseguimos la conexión (getConnection) y hacemos la llamada al procedimiento almacenado (prepareCall):	
 		try (Connection con = ConnectionManager.getConnection();
 				CallableStatement cs = con.prepareCall("{ CALL pa_categoria_update(?, ?) }")) {
 
 			//parámetros de entrada (1ª y 2ª ?):
-			cs.setString(1, pojo.getNombre());
-			cs.setInt(2, id);
+			//OJO con el orden de los parámetros: en la SQL tenemos p_id como parámetro 1 y p_nombre como parámetro 2, hay que ponerlos igual
+			cs.setInt(1, id);
+			cs.setString(2, pojo.getNombre());
 			
-			//parámetro de salida (2ª ?), será el id de la categoría:
-			cs.registerOutParameter(2, java.sql.Types.INTEGER);
-			
-			LOG.debug(cs);
+			LOG.debug(cs);			
 			
 			//ejecutamos el procedimiento almacenado executeUpdate, CUIDADO porque NO es una SELECT --> executeQuery:
-			cs.executeUpdate();
-			
-			//una vez ejecutado, podemos recuperar el parámetro de salida (2ª ?):
-			pojo.setId(cs.getInt(2));
-			
+			if(cs.executeUpdate() == 1) {
+				pojo.setId(id);
+			}else {
+				throw new Exception("No se puede modificar el registro " + pojo + " por id " + id);
+			}
 
 		}
 		 
-		return pojo;
+		return registro;
 	}
 
 	@Override
@@ -187,6 +188,17 @@ public class CategoriaDAO implements ICategoriaDAO {
 		}	
 			
 		return registro;
+	}
+	
+	
+	//mapper:
+	private Categoria mapper(ResultSet rs) throws SQLException{
+		
+		Categoria c = new Categoria();
+		c.setId(rs.getInt("id"));
+		c.setNombre(rs.getString("nombre"));
+		
+		return c;
 	}
 
 }
